a48b3fb78be00cea333bb758bc005cd01a2228c5
Stephen Brundage <sbrundage@Stephens-MacBook-Pro.local>
2019-09-25 00:36:30 -0400

    Add Podfile
    - Add Alamofire 5 dependency to make network requests

---
+++ CryptoPriceTracker/CryptoPriceTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoPriceTracker
+//
+//  Created by Stephen Brundage on 9/25/19.
+//  Copyright © 2019 Stephen Brundage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
