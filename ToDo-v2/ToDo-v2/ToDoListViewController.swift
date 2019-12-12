ad77a28eb974050784f1959a7591a0abbab9ffc3
Stephen Brundage <stephen.brundage12@gmail.com>
2019-12-12 08:37:37 -0500

    Initial Commit & TableView Setup

---
+++ ToDo-v2/ToDo-v2/ToDoListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ToDo-v2
+//
+//  Created by Stephen Brundage on 12/12/19.
+//  Copyright © 2019 Stephen Brundage. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemsArray = ["Learn More Swift!", "Call Mom", "XMas Gifts"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemsArray[indexPath.row]
+        
+        return cell
+    }
+}
+
