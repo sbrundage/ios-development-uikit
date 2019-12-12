f4f3794dc9f99c72bd42b4ea5ff911c809aef377
Stephen Brundage <stephen.brundage12@gmail.com>
2019-12-12 09:14:42 -0500

    Add items to list functionality - Items aren't persistent

--- ToDo-v2/ToDo-v2/ToDoListViewController.swift
+++ ToDo-v2/ToDo-v2/ToDoListViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemsArray = ["Learn More Swift!", "Call Mom", "XMas Gifts"]
+    var itemsArray = ["Learn More Swift!", "Call Mom", "XMas Gifts"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - TableView Datasouce Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemsArray.count
     }
@@ -27,6 +28,43 @@ class ToDoListViewController: UITableViewController {
         cell.textLabel?.text = itemsArray[indexPath.row]
         
         return cell
+    }
+    
+    // MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add New To Do Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+
+            if let newToDo = alert.textFields?.first?.text {
+                self.itemsArray.append(newToDo)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
