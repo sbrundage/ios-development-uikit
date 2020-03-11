327a58b5f76b573db304400bc7118fb8c120d5fc
Stephen Brundage <stephen.brundage12@gmail.com>
2020-03-11 09:32:59 -0400

    Add ARCL Pod - Add podfile with ARCL library - Use MapKit to search for location destinations - Use these found destination locations and create annotation nodes - Annotation nodes are placed in sceneview to be displayed in AR

--- ARKitCoreLocation/ARKitCoreLocation/ARViewController.swift
+++ ARKitCoreLocation/ARKitCoreLocation/ARViewController.swift
@@ -8,19 +8,38 @@
 
 import UIKit
 import CoreLocation
+import MapKit
+import ARCL
 
 class ARViewController: UIViewController {
     
     var destination: String!
     
     lazy private var locationManager = CLLocationManager()
+    var sceneLocationView = SceneLocationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = destination
         
+        sceneLocationView.run()
+        view.addSubview(sceneLocationView)
+        
         setupLocationManager()
+        findLocalPlaces()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        sceneLocationView.frame = view.bounds
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        sceneLocationView.pause()
     }
     
     private func setupLocationManager() {
@@ -28,6 +47,45 @@ class ARViewController: UIViewController {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
+    }
+    
+    private func findLocalPlaces() {
+        // Get current location of user
+        guard let location = locationManager.location else { return }
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = destination
+        
+        var region = MKCoordinateRegion()
+        region.center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
+        request.region = region
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            // To Do: Handle Error
+            // For now, we will just return
+            if error != nil { return }
+            
+            // Make sure response is not nil
+            guard let response = response else { return }
+            
+            for item in response.mapItems {
+                if let placeLocation = item.placemark.location {
+                    self.createAnnotationNode(for: placeLocation)
+                }
+            }
+        }
+    }
+    
+    private func createAnnotationNode(for location: CLLocation) {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let location = CLLocation(coordinate: coordinate, altitude: 300)
+        
+        let pin = UIImage(named: "pin")!
+        let annotationNode = LocationAnnotationNode(location: location, image: pin)
+        
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
     }
 }
 
